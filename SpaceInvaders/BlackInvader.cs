91647cf6ef75cce7a7ead75f1858b5fa6c208aff
Kristebel <45570200+Kristebel@users.noreply.github.com>
2018-12-03 19:41:39 +0200

    Create BlackInvader.cs

---
+++ SpaceInvaders/BlackInvader.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    public partial class BlackInvader : UserControl
+    {
+        public BlackInvader()
+        {
+            InitializeComponent();
+        }
+        public int Speed { get { return 20; } }
+        public int Move { get { return 30; } }
+    }
+}
