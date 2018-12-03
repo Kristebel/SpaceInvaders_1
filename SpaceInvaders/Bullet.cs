dbe260d1bd9d9075dafdf610ec745496228a16a3
Kristebel <45570200+Kristebel@users.noreply.github.com>
2018-12-03 19:41:46 +0200

    Create Bullet.cs

---
+++ SpaceInvaders/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    public partial class Bullet : UserControl
+    {
+        public Bullet()
+        {
+            InitializeComponent();
+        }
+    }
+}
