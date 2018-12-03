ae34b22ca540a5923f032749bcfcc8b6133b9830
Kristebel <45570200+Kristebel@users.noreply.github.com>
2018-12-03 19:42:52 +0200

    Create Shooter.cs

---
+++ SpaceInvaders/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    public partial class Shooter: UserControl
+    {
+        private const int _speed = 30; 
+        public Shooter()
+        {
+            InitializeComponent();
+        }
+        public int Speed { get { return _speed; } }
+    }
+}
