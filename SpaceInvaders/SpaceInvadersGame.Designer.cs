aa84d1b4de4599148f2e29851a314da57b496533
Kristebel <45570200+Kristebel@users.noreply.github.com>
2018-12-03 19:43:04 +0200

    Create SpaceInvadersGame.Designer.cs

---
+++ SpaceInvaders/SpaceInvadersGame.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SpaceInvaders
+{
+    partial class SpaceInvadersGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.scoreCount = new System.Windows.Forms.Label();
+            this.bgw = new System.ComponentModel.BackgroundWorker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(784, 380);
+            this.gamePanel.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 389);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(41, 13);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score: ";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // scoreCount
+            // 
+            this.scoreCount.AutoSize = true;
+            this.scoreCount.Location = new System.Drawing.Point(46, 389);
+            this.scoreCount.Name = "scoreCount";
+            this.scoreCount.Size = new System.Drawing.Size(10, 13);
+            this.scoreCount.TabIndex = 3;
+            this.scoreCount.Text = "-";
+            // 
+            // bgw
+            // 
+            this.bgw.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw_DoWork);
+            this.bgw.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw_RunWorkerCompleted);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(372, 384);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // SpaceInvadersGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.scoreCount);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.gamePanel);
+            this.MaximumSize = new System.Drawing.Size(800, 450);
+            this.MinimumSize = new System.Drawing.Size(800, 450);
+            this.Name = "SpaceInvadersGame";
+            this.Text = "Space Invaders";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SpaceInvaders_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label scoreCount;
+        private System.ComponentModel.BackgroundWorker bgw;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
